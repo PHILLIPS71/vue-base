19c2304b9f1382965dff837ab772fa7451267e4d
Jordan Phillips <jordanphillips@Jordans-MacBook-Pro-3.local>
2016-10-10 17:16:47 +1100

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+module.exports = {
+    entry: SRC_DIR + '/app/index.js',
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
+    }
+}
